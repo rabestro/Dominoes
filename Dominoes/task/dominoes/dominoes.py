b6648729697afc4cd135d7aeee36587c701b0c74
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-04-08 18:42:50 +0300

    Solved stage 1

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -1,11 +1,36 @@
-# Write your code here
+import random
+
+
 def generate_stock():
-    stock = []
+    generated_stock = []
     for i in range(7):
         for j in range(0, i + 1):
-            stock.append([j, i])
-    return stock
+            generated_stock.append([j, i])
+    return generated_stock
 
 
-stock = generate_stock()
-print(stock)
+stock_pieces = generate_stock()
+# print(stock_pieces, len(stock_pieces))
+random.shuffle(stock_pieces)
+
+player_pieces = stock_pieces[0:7]
+computer_pieces = stock_pieces[7:14]
+stock_pieces = stock_pieces[14:]
+domino_snake = []
+# is_player_move = False
+
+for i in range(6, -1, -1):
+    piece = [i, i]
+    if piece in computer_pieces or piece in player_pieces:
+        domino_snake.append(piece)
+        if piece in computer_pieces:
+            computer_pieces.remove(piece)
+        else:
+            player_pieces.remove(piece)
+        break
+
+print('Stock pieces:', stock_pieces)
+print('Computer pieces:', computer_pieces)
+print('Player pieces:', player_pieces)
+print('Domino snake:', domino_snake)
+print('Status:', 'player' if len(computer_pieces) == 6 else 'computer')
